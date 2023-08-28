2eca654fe69e1d81087e1c31a107bf814828aa77
ivoorusso <129088802+ivoorusso@users.noreply.github.com>
2023-08-28 16:32:45 -0300

    Update ComboBox.jsx

--- src/components/ComboBox.jsx
+++ src/components/ComboBox.jsx
@@ -1,38 +1,66 @@
-import { useState } from 'react'
-import PropTypes from 'prop-types'
-import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import { Combobox } from '@headlessui/react'
+import { useState, useEffect } from "react";
+import PropTypes from "prop-types";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { Combobox } from "@headlessui/react";
 
 const peopleMock = [
-  { id: 1, name: 'Leslie Alexander' },
+  { id: 1, name: "Leslie Alexander" },
   // More users...
-]
+];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
-export default function ComboBox({ items = peopleMock, title, selectedPerson, setSelectedPerson }) {
-  const [query, setQuery] = useState('')
+export default function ComboBox({
+  items = peopleMock,
+  title,
+  selectedPerson,
+  setSelectedPerson,
+}) {
+  const [query, setQuery] = useState("");
+  const [showCustomProviderField, setShowCustomProviderField] = useState(false);
+  const [customProvider, setCustomProvider] = useState("");
+
+  useEffect(() => {
+    if (query === "") {
+      setSelectedPerson(null);
+    }
+  }, [query]);
+
+  const handleInputBlur = () => {
+    setQuery(""); // restablece el estado del query
+  };
 
   const filteredPeople =
-    query === ''
+    query === ""
       ? items
       : items.filter((person) => {
-          return person.name.toLowerCase().includes(query.toLowerCase())
-        })
+          return person.name.toLowerCase().includes(query.toLowerCase());
+        });
 
   return (
-    <Combobox as="div" value={selectedPerson} onChange={setSelectedPerson} className="mb-4">
-      <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">{title}</Combobox.Label>
+    <Combobox
+      as="div"
+      value={selectedPerson}
+      onChange={setSelectedPerson}
+      className="mb-4"
+    >
+      <Combobox.Label className="block text-sm font-medium leading-6 text-gray-900">
+        {title}
+      </Combobox.Label>
       <div className="relative mt-2">
         <Combobox.Input
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
+          onBlur={handleInputBlur}
           displayValue={(person) => person?.name}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
-          <ChevronUpDownIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
+          <ChevronUpDownIcon
+            className="h-5 w-5 text-gray-400"
+            aria-hidden="true"
+          />
         </Combobox.Button>
 
         {filteredPeople.length > 0 && (
@@ -43,20 +71,27 @@ export default function ComboBox({ items = peopleMock, title, selectedPerson, se
                 value={person}
                 className={({ active }) =>
                   classNames(
-                    'relative cursor-default select-none py-2 pl-3 pr-9',
-                    active ? 'bg-indigo-600 text-white' : 'text-gray-900'
+                    "relative cursor-default select-none py-2 pl-3 pr-9",
+                    active ? "bg-indigo-600 text-white" : "text-gray-900"
                   )
                 }
               >
                 {({ active, selected }) => (
                   <>
-                    <span className={classNames('block truncate', selected && 'font-semibold')}>{person.name}</span>
+                    <span
+                      className={classNames(
+                        "block truncate",
+                        selected && "font-semibold"
+                      )}
+                    >
+                      {person.name}
+                    </span>
 
                     {selected && (
                       <span
                         className={classNames(
-                          'absolute inset-y-0 right-0 flex items-center pr-4',
-                          active ? 'text-white' : 'text-indigo-600'
+                          "absolute inset-y-0 right-0 flex items-center pr-4",
+                          active ? "text-white" : "text-indigo-600"
                         )}
                       >
                         <CheckIcon className="h-5 w-5" aria-hidden="true" />
@@ -67,10 +102,11 @@ export default function ComboBox({ items = peopleMock, title, selectedPerson, se
               </Combobox.Option>
             ))}
           </Combobox.Options>
+          
         )}
       </div>
     </Combobox>
-  )
+  );
 }
 
 ComboBox.propTypes = {
