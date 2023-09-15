ca35d35f1a92864e39526ac9fdb2468f02e081f5
ivoorusso <ivan@setandforget.io>
2023-09-15 18:15:18 -0300

    ComboboxGroup

--- src/components/ComboBoxGroup.jsx
+++ src/components/ComboBoxGroup.jsx
@@ -48,8 +48,13 @@ export default function ComboBoxGroup({ items = peopleMock, title, selectedPerso
           className="w-full rounded-md border-0 bg-white py-1.5 pl-3 pr-10 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           onChange={(event) => setQuery(event.target.value)}
           displayValue={(person) => person?.name}
-          onBlur={() => {
-            setQuery('');  
+          onBlur={(e) => {
+            if (
+              !e.relatedTarget ||
+              !e.relatedTarget.classList.contains("optionclass")
+            ) {
+              setQuery("");
+            }
           }}
         />
         <Combobox.Button className="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
@@ -72,7 +77,7 @@ export default function ComboBoxGroup({ items = peopleMock, title, selectedPerso
                     value={person}
                     className={({ active }) =>
                       classNames(
-                        'relative cursor-default select-none py-2 pl-3 pr-9',
+                        'relative cursor-default select-none py-2 pl-3 pr-9 optionclass',
                         active ? 'bg-emerald-700 text-white' : 'text-gray-900'
                       )
                     }
